6a7ecb11acb652f41cfef7c8cbf403985f2ec9f5
RajAman01 <Raj.aman4455@gmail.com>
2021-09-20 20:51:27 +0530

    Add project files.

---
+++ CatClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CatClass
+{
+    class cat
+    {
+        public string name;
+        public int age;
+        public string speak;
+
+        public  cat(string _name,int _age)
+        {
+            name = _name;
+            age = _age;
+        }
+
+        public void CastSpell()
+        {
+            Console.WriteLine(name + " " + age + "yr old" + "Speaks"+speak);
+           
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cat cat01 = new cat();
+            cat01.name = "foster";
+            cat01.age = 20;
+            cat01.speak = "Meawo";
+            cat01.CastSpell();
+        }
+    }
+}
